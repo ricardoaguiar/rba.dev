e71d8d27eba31f61e093f29b59dd746d47e46e77
ricardo aguiar <barrios.dev@gmail.com>
2020-06-06 18:53:06 +0200

    added contentful

--- src/components/post.js
+++ src/components/post.js
@@ -1,37 +1,51 @@
 import React from "react"
 import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const getPosts = graphql`
   {
-    post: allContentfulAsset {
+    post: allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
       edges {
         node {
-          id
           title
-          description
+          slug
+          publishDate(formatString: "MMMM Do, YYYY")
+          tags
+          heroImage {
+            fluid {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+          text: description {
+            description
+          }
         }
       }
     }
   }
 `
+
 const FlexContainer = styled.div`
   display: flex;
+  justify-content: center;
   padding: 5rem;
   background-color: #88765444;
+  border-bottom: 1px solid #99876522;
 `
 const FlexItem = styled.div`
-  margin: 0 auto;
   width: 75vw;
   height: 50vh;
   background-color: #88765422;
   border: 0.5px solid #88765433;
+  padding: 1rem 3rem;
+`
+const Image = styled.div`
+  max-width: 20vw;
 `
 
 const Post = () => {
   const { post } = useStaticQuery(getPosts)
-
-  console.log("post", post)
 
   return (
     <>
@@ -40,7 +54,11 @@ const Post = () => {
           <FlexContainer key={node.id}>
             <FlexItem>
               <p>Title = {node.title}</p>
-              <p>Description = {node.description}</p>
+              <Image>
+                <Img fluid={node.heroImage.fluid} alt={node.title} />
+              </Image>
+              <p>{node.slug}</p>
+              <p>{node.text.description}</p>
             </FlexItem>
           </FlexContainer>
         )
