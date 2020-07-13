d3195afc54ff9c2ffbb3b617fd8997b60d30c999
ricardo aguiar <barrios.dev@gmail.com>
2020-07-13 13:58:34 +0200

    removed gatsby-transition-link plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-transition-link`,
     `gatsby-plugin-svgr`,
     {
       resolve: `gatsby-source-filesystem`,
