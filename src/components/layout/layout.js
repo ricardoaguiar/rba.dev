08f760d8b43b207056faee591ccb7843165229fa
ricardo aguiar <barrios.dev@gmail.com>
2020-07-13 20:39:25 +0200

    fix layout

---
+++ src/components/layout/layout.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+import "./styles.css"
+
+const Layout = ({ children }) => {
+  return <main>{children}</main>
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
