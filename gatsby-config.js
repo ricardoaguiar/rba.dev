5125b819f56ca9211870101dc1a0044ade231076
ricardo aguiar <barrios.dev@gmail.com>
2020-07-13 17:47:24 +0200

    changed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,9 +36,6 @@ module.exports = {
         icon: `src/images/rba-dev-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
     {
       resolve: "gatsby-source-contentful",
       options: {
