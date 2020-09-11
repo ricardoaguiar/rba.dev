5f6bc3406ddcd568539aba70f9855b940113bcd2
Ricardo Aguiar <ricardoaguiar@rba.local>
2020-09-11 22:19:46 +0200

    added header to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
 import React from "react"
 
-// import Wrapper from "../components/wrapper/wrapper"
+import Header from "../components/header/header"
+
 import SEO from "../components/seo"
 import NotFound from "../components/not-found"
 
 const NotFoundPage = () => (
   <>
+    <Header />
     <SEO title="404: Not found" />
     <NotFound />
   </>
 )
+
 
 export default NotFoundPage
