1269c6dd53cf2e6b2e6eaeae64b7cce06c3a1e8c
Ricardo Aguiar <ricardoaguiar@rba.local>
2020-09-12 09:56:24 +0200

    added wrapper to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
 import React from "react"
+import "./styles.css"
 import Wrapper from "../components/wrapper/wrapper"
 import Header from "../components/header/header"
-
+import Footer from "../components/footer/footer"
 import SEO from "../components/seo"
 import NotFound from "../components/not-found"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+
 
 const NotFoundPage = () => (
   <>
@@ -11,6 +15,7 @@ const NotFoundPage = () => (
     <Header />
     <SEO title="404: Not found" />
     <NotFound />
+    <Footer />
   </Wrapper>
   </>
 )
