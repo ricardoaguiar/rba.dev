c378ad31c24a99a7fa69fdfd8580839061683e81
ricardo aguiar <barrios.dev@gmail.com>
2020-09-16 09:50:37 +0200

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,13 +3,13 @@ import styled from "@emotion/styled"
 
 const FooterSection = styled.section`
   min-width: 100vw;
-  min-height: 10vh;
+  min-height: 5vh;
   background: var(--rbadev-duotone-4-hex);
   display: flex;
   flex-direction: row;
   margin: 0 auto;
   bottom: 0;
-  padding: 2em;
+  padding:.5em;
 `
 
 const Footer = () => {
