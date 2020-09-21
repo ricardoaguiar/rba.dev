744da7d62e4a378d1c9ee6515e00130efccf1b04
ricardo aguiar <barrios.dev@gmail.com>
2020-09-21 09:51:23 +0200

    Update social-media-menu.js

--- src/components/social/social-media-menu.js
+++ src/components/social/social-media-menu.js
@@ -142,7 +142,7 @@ const SocialMediaMenu = () => {
           `}
         >
           <FontAwesomeIcon
-            icon={["far", "fa-envelop"]}
+            icon={["far", "envelop"]}
             size="2x"
             pointerEvents="none"
           />
