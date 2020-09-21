63a8d18b3f4da381588e32cf18e8e48ce4ccf874
ricardo aguiar <barrios.dev@gmail.com>
2020-09-21 08:23:55 +0200

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+import "./styles.css"
+import Wrapper from "../components/wrapper/wrapper"
+import Header from "../components/header/header"
+import Footer from "../components/footer/footer"
+import SEO from "../components/seo"
+import NotFound from "../components/not-found"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+library.add(fab)
+
+const About = () => (
+  <>
+  <Wrapper>
+    <Header />
+    <SEO title="About" />
+    Something about this guy.
+    <Footer />
+  </Wrapper>
+  </>
+)
+
+
+export default About
