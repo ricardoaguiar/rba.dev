c84696d40163fc54f12529b7bf5222f510963482
ricardo aguiar <barrios.dev@gmail.com>
2020-10-28 11:01:06 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,6 +17,8 @@ export const HomeContainer = styled.header`
   padding: 1vh 0 0 5vw;
   background: var(--rbadev-mono-5-hex);
   align-items: center;
+border-right: 1px solid var(--rbadev-mono-1-hex);
+border-bottom: 1px solid var(--rbadev-mono-1-hex);
   `};
 `
 
