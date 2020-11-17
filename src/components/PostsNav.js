c4b91d4eaf6d105690db5af50c216206fea50131
ricardo aguiar <barrios.dev@gmail.com>
2020-11-17 07:08:49 +0100

    Update PostsNav.js

--- src/components/PostsNav.js
+++ src/components/PostsNav.js
@@ -18,7 +18,7 @@ const getPosts = graphql`
             shortDescription
           }
           heroImage {
-            fluid(maxHeight: 800, maxWidth: 600) {
+            fluid(maxHeight: 1000, maxWidth: 1800) {
               ...GatsbyContentfulFluid
             }
           }
@@ -30,7 +30,7 @@ const getPosts = graphql`
 // grid
 const GridContainer = styled.nav`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(380px, 1fr));
   grid-template-rows: 1fr;
   grid-gap: 20px;
   justify-content: center;
@@ -42,8 +42,9 @@ const GridContainer = styled.nav`
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     `}
   ${respondTo.T900`
-    grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
-    margin-left: 70px;
+    grid-template-columns: repeat(auto-fit, minmax(650px, 1fr));
+    margin-left: 90px;
+    margin-right: 20px;
   `}
 `
 // grid item / cell
