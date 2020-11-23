67bc956bb7bf00665410ec4fab68263af464e2e7
ricardo aguiar <barrios.dev@gmail.com>
2020-11-23 10:15:39 +0100

    Update skills.js

--- src/components/skills.js
+++ src/components/skills.js
@@ -11,24 +11,25 @@ const SkillContainer = styled.ul`
 
 const Skills = () => {
     return (
+   <>
+   <h4>Skills:</h4>
    <SkillContainer>
-    <li>Skills:</li>
     <li>React</li>
     <li>JavaScript</li>
     <li>ES6</li>
+    <li>Styled Components</li>
     <li>NodeJS</li>
-    <li>Gatsby</li>
-    <li>AWS Lightsail</li>
     <li>Git</li>
-    <li>Github</li>
+    <li>Swagger</li>   
+    <li>Gatsby</li>
+    <li>Contentful</li>
     <li>CSS3</li>
     <li>HTML5</li>
-    <li>KnexJS</li>
     <li>MySQL</li>
     <li>Photography</li>
     <li>Photoshop</li>
-    <li>Lightroom</li>
    </SkillContainer>
+  </>
     )
 }
 
