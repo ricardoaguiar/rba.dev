99c335745fa56ac1e7cfcb9109c27444622d454c
ricardo aguiar <barrios.dev@gmail.com>
2020-11-23 09:19:43 +0100

    added skills.js

---
+++ src/components/skills.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Skills = () => {
+    return (
+        <ul>
+    <li>Skills:</li>
+    <li>React</li>
+    <li>JavaScript</li>
+    <li>ES6</li>
+    <li>NodeJS</li>
+    <li>Gatsby</li>
+    <li>AWS Lightsail</li>
+    <li>Git</li>
+    <li>Github</li>
+    <li>CSS3</li>
+    <li>HTML5</li>
+    <li>KnexJS</li>
+    <li>MySQL</li>
+    <li>Photography</li>
+    <li>Photoshop</li>
+    <li>Lightroom</li>
+        </ul>
+    )
+};
+
+export default Skills;
