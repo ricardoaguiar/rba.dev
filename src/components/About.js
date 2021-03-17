5e0a1271401a377cc96701a9bf9ef305eb9da8b9
ricardo aguiar <barrios.dev@gmail.com>
2021-03-17 06:15:39 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -34,11 +34,11 @@ function AboutPage () {
         <li>
 
 
-// I’m Ricardo. I grew up in Brazil, and at the age of 26, I moved to New York City, where I would live for the next 13 years and then 7 more years in Miami, before moving to Denmark with my wife. 
+    {* I’m Ricardo. I grew up in Brazil, and at the age of 26, I moved to New York City, where I would live for the next 13 years and then 7 more years in Miami, before moving to Denmark with my wife. 
 
-// During my time in the USA, I’ve worked in food, advertising, and financing. Some were tech-related.  While working in advertising in New York, I developed and tested HTML email campaigns and MVPs for Citibank, GE, and Pitney Bowes companies. 
+During my time in the USA, I’ve worked in food, advertising, and financing. Some were tech-related.  While working in advertising in New York, I developed and tested HTML email campaigns and MVPs for Citibank, GE, and Pitney Bowes companies. 
 
-// To refresh my skills, I recently graduated from the seven-month intense Hack Your Future Bootcamp for full-stack web development in April 2020. 
+To refresh my skills, I recently graduated from the seven-month intense Hack Your Future Bootcamp for full-stack web development in April 2020. *}
 
         </li>
       </BlogList>
