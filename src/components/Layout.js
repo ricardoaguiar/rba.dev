27a140fc0cb3a4c1e52be6964ba22697692b512c
devrba <barrios.dev@gmail.com>
2021-03-27 17:36:05 +0100

    moved files

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Nav from "../components/Nav/Nav"
-import Footer from "../components/Footer/Footer"
+import React from 'react'
+import PropTypes from 'prop-types'
+import Nav from './common/nav'
+import Footer from './common/footer'
 
 const Layout = ({ children }) => {
   return (
